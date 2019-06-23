e2ada1041beea106307ab8c24d5dbef71c4b9f08
Chelsea Owiti <chelseaowiti@gmail.com>
2019-06-23 18:22:36 -0400

    news source class defined

--- app/models.py
+++ app/models.py
@@ -1,5 +1,9 @@
-# class Newssource:
-#   """
-#   class to define newssource objects
-#   """
-#   def __init__(self, id, name, auth)
+class Newssource:
+  """
+  class to define news objects
+  """
+  def __init__(self, id, name, description, url):
+    self.id = id
+    self.name = name 
+    self.description = description
+    self.url = url
