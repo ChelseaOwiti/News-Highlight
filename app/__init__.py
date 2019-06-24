46f8c1437236fd600fcf922f030005d6f12feeb6
Chelsea Owiti <chelseaowiti@gmail.com>
2019-06-23 23:51:08 -0400

    newws article class added

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,25 @@
 from flask import Flask
-from .config import DevConfig
+from config import config_options
 from flask_bootstrap import Bootstrap
 
-app = Flask(__name__, instance_relative_config= True)
 
-#setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+bootstrap = Bootstrap()
 
-#flask extensions
-bootstrap = Bootstrap(app)
+def create_app(config_name):
+  app = Flask(__name__)
 
+  #create app configurations
+  app.config.from_object(config_options[config_name])
 
-from app import views
+  #initialize flask extensions
+  bootstrap.init_app(app)
+
+  #register blueprint
+  from .main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
+
+  #setting config
+  from .request import configure_request
+  configure_request(app)
+
+  return app 
